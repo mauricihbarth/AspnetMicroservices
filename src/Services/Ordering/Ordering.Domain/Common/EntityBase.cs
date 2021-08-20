1066c17cf7d871f2d945f9089b6b11bb95ddc9c3
mauricihbarth <mauricihbarth>
2021-08-20 16:56:39 -0300

    Preparing appplication layer structute

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ordering.Domain.Commom
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+
+
+    }
+}
